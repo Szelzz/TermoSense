662347a25156aa042dda259aed002878ee404b76
Szel <szel@eslz.net>
2021-06-30 00:50:47 +0200

    webserver

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <DHT_U.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <ESP8266WiFi.h>
 
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS D7
@@ -15,6 +16,8 @@
 #define RED_LED 5   // D1
 #define GREEN_LED 4 // D2
 
+#define HOSTNAME "TEMPERATURA"
+
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
 
@@ -24,39 +27,15 @@ DHT_Unified dht(DHTPIN, DHTTYPE);
 
 uint32_t delayMS;
 
-void setup()
-{
-  Serial.begin(115200);
-  Serial.println("Start");
-  pinMode(GREEN_LED, OUTPUT);
-  pinMode(RED_LED, OUTPUT);
-  sensors.begin();
-  // Initialize device.
-  dht.begin();
-  sensor_t sensor;
-  dht.temperature().getSensor(&sensor);
-  // Serial.println(F("------------------------------------"));
-  // Serial.println(F("Temperature Sensor"));
-  // Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-  // Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-  // Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  // Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
-  // Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
-  // Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
-  // Serial.println(F("------------------------------------"));
-  // // Print humidity sensor details.
-  // dht.humidity().getSensor(&sensor);
-  // Serial.println(F("Humidity Sensor"));
-  // Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-  // Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-  // Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  // Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
-  // Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
-  // Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
-  // Serial.println(F("------------------------------------"));
-  // Set delay between sensor readings based on sensor details.
-  delayMS = (sensor.min_delay / 1000) + 1000;
-}
+WiFiServer server(80);
+
+IPAddress local_IP(192, 168, 0, 100);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+float insideTemp;
+float outsideTemp;
 
 float insideTemperature()
 {
@@ -68,18 +47,6 @@ float insideTemperature()
     return DEVICE_DISCONNECTED_C;
   }
   return event.temperature;
-  // Get humidity event and print its value.
-  // dht.humidity().getEvent(&event);
-  // if (isnan(event.relative_humidity))
-  // {
-  //   Serial.println(F("Error reading humidity!"));
-  // }
-  // else
-  // {
-  //   Serial.print(F("Humidity: "));
-  //   Serial.print(event.relative_humidity);
-  //   Serial.println(F("%"));
-  // }
 }
 
 float outsideTemperature()
@@ -91,25 +58,100 @@ float outsideTemperature()
   return sensors.getTempCByIndex(0);
 }
 
+void serveWebpage()
+{
+
+  WiFiClient client = server.available();
+  if (client)
+  {
+
+while (client.connected()) {
+
+      if (client.available()) {
+
+        char c = client.read();
+
+
+      Serial.println("Connected to client");
+
+      String line = client.readStringUntil('\r');
+      Serial.println(line);
+
+
+      client.print("HTTP/1.1 200 OK\r\n");
+      client.print("Host: 192.168.0.100\r\n");
+      client.print("Content-type: text/html; charset=utf-8\r\n");
+      client.print("Connection: close\r\n");
+      client.print("\r\n");
+      client.println("<html>");
+      client.print("<p>W domu: ");
+      client.print(insideTemp);
+      client.println("°C</p>");
+      client.print("<p>W ogródku: ");
+      client.print(outsideTemp);
+      client.println("°C</p>");
+      client.println("</html>");
+      client.print("\r\n");
+    }
+
+    // close the connection:
+    client.stop();
+  }
+}
+void setup()
+{
+  Serial.begin(115200);
+
+  WiFi.hostname(HOSTNAME);
+  if (!WiFi.config(local_IP, gateway, subnet))
+  {
+    Serial.println("STA Failed to configure");
+  }
+  WiFi.begin("o==[]:::::::::::::::::::::::::>", "Mikrofala2021!");
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Start");
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  sensors.begin();
+  // Initialize device.
+  dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  // Set delay between sensor readings based on sensor details.
+  delayMS = (sensor.min_delay / 1000) + 1000;
+
+  server.begin();
+}
+
 void loop()
 {
   // Delay between measurements.
   delay(delayMS);
 
-  float inside = insideTemperature();
-  float outside = outsideTemperature();
+  insideTemp = insideTemperature();
+  outsideTemp = outsideTemperature();
 
   Serial.print("Inside: ");
-  Serial.println(inside);
+  Serial.println(insideTemp);
   Serial.print("Outside: ");
-  Serial.println(outside);
+  Serial.println(outsideTemp);
 
-  if (inside == DEVICE_DISCONNECTED_C || outside == DEVICE_DISCONNECTED_C)
+  if (insideTemp == DEVICE_DISCONNECTED_C || outsideTemp == DEVICE_DISCONNECTED_C)
   {
     digitalWrite(GREEN_LED, HIGH);
     digitalWrite(RED_LED, HIGH);
   }
-  else if (inside > (outside + 0.7))
+  else if (insideTemp > (outsideTemp - 0.7))
   {
     digitalWrite(GREEN_LED, HIGH);
     digitalWrite(RED_LED, LOW);
@@ -119,4 +161,6 @@ void loop()
     digitalWrite(GREEN_LED, LOW);
     digitalWrite(RED_LED, HIGH);
   }
+
+  serveWebpage();
 }
